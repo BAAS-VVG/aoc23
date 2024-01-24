5f3ed891a6ee53b94148e25cf24617384cf678ee
BAAS-VVG <baas@csvvg.eu>
2024-01-24 15:47:12 +0100

    d24 part 1

--- d24.py
+++ d24.py
@@ -26,3 +26,29 @@ def d24():
 
     print(res)
 
+    # part 2
+
+    hailstones = []
+    with open(inputfile) as f:
+        for line in f.readlines():
+            pos, vec = [[int(c) for c in v.split(',')] for v in line.strip().split('@')]
+            hailstones.append(Ray(pos, [p + d for p, d in zip(pos, vec)]))
+
+    # find x, y, z, vx, vy, vz from all inputs as a function of t
+    # Rp + t1 * Rv = H1p + t1 * H1v
+    # Rp + t2 * Rv = H2p + t2 * H2v
+    # Rp + t3 * Rv = H3p + t3 * H3v
+    # Rp = H1p + t1 * (-Rv + H1v)
+    # Rp = H2p + t2 * (-Rv + H2v)
+    # Rp = H3p + t3 * (-Rv + H3v)
+    #
+    # H1p + t1 * (-Rv + H1v) == H2p + t2 * (-Rv + H2v)
+    # H1p - H2p == t2 * (-Rv + H2v) - t1 * (-Rv + H1v)
+    # Rv = (t1 * H1v - t2 * H2v + H1p - H2p) / (t1 - t2)
+
+
+
+
+
+
+
